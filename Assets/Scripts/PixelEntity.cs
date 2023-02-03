c2077792c20de91c22b085f63878007516bed340
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2023-02-03 19:12:09 +0200

    PixelEntity base

---
+++ Assets/Scripts/PixelEntity.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public bool IsLiquid => isLiquid;
+
+    [SerializeField] private bool isLiquid = false;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}

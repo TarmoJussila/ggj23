e9a3624317caeee9bcf92ea4ed70c4266459daa7
TuomoO <tuomo.oksanen@zaibatsu.fi>
2023-02-05 14:40:39 +0200

    Change resolution button

--- Assets/UI/Settings/Settings.cs
+++ Assets/UI/Settings/Settings.cs
@@ -6,20 +6,22 @@ namespace Klonk.UI.Settings
 {
     public class MSettings : MonoBehaviour
     {
+        private DropdownField _resolutionDropdown;
+        
         private void OnEnable()
         {
             UIDocument menu = GetComponent<UIDocument>();
             VisualElement root = menu.rootVisualElement;
 
-            DropdownField resolutionDropdown = root.Q<DropdownField>("ResolutionsDropDown");
-            resolutionDropdown.choices.Clear();
+            _resolutionDropdown = root.Q<DropdownField>("ResolutionsDropDown");
+            _resolutionDropdown.choices.Clear();
             Resolution[] resolutions = Screen.resolutions;
 
             foreach (Resolution resolution in resolutions)
             {
-                resolutionDropdown.choices.Add(resolution.width + "x" + resolution.height + " " + resolution.refreshRate + "Hz");
+                _resolutionDropdown.choices.Add(resolution.width + "x" + resolution.height + " " + resolution.refreshRate + "Hz");
             }
-            
+
             root.Q<Button>("BackButton").RegisterCallback<ClickEvent>(_ =>
             {
                 SceneManager.LoadScene("Menu");
@@ -28,6 +30,21 @@ namespace Klonk.UI.Settings
             root.Q<Button>("FullScreenButton").RegisterCallback<ClickEvent>(_ =>
             {
                 Screen.fullScreen = !Screen.fullScreen;
+            });
+            
+            root.Q<Button>("ChangeButton").RegisterCallback<ClickEvent>(_ =>
+            {
+                int index = _resolutionDropdown.index;
+                if (index < Screen.resolutions.Length)
+                {
+                    Resolution resolution = Screen.resolutions[_resolutionDropdown.index];
+                    Debug.Log($"Switching to resolution: {resolution}");
+                    Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen, resolution.refreshRate);
+                }
+                else
+                {
+                    Debug.LogWarning("Failed to switch resolution");
+                }
             });
         }
     }
